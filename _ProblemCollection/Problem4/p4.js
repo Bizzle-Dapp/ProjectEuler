65d53d2a16d52f2825872ba18e77ddf3a68715bf
Bizzle Dapp <36421291+Bizzle-Dapp@users.noreply.github.com>
2020-06-18 22:25:13 +0100

    Problem4 completed. Runs in 210 milliseconds - could probably be faster.

--- _ProblemCollection/Problem4/p4.js
+++ _ProblemCollection/Problem4/p4.js
@@ -4,3 +4,37 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
+
+isPalindrome = (number) => {
+    // Convert number to string
+    let convertedNumber = String(number);
+    // Convert to array for reversal
+    let convertNumberToArray = String(number).split('');
+    // Prepare our reversed "number"
+    let reversedNumber = "";
+    convertNumberToArray.reverse().map((c) => {
+        reversedNumber += c;
+    })
+    // Compare
+    return (convertedNumber === reversedNumber);
+}
+
+findLargestPalindrome = (minValue, maxValue) => {
+    let startTime = Date.now();
+    let largestPalindrome = 0;
+    for(i = minValue; i <= maxValue; i++)
+    {
+        for(l = minValue; l <= maxValue; l++)
+        {
+            if(isPalindrome((i*l)) && (i*l) > largestPalindrome)
+            {
+                largestPalindrome = (i*l);
+            }
+        }
+    }
+    let endTime = Date.now();
+    console.log("Completed in: " + (endTime - startTime) + " milliseconds.");
+    console.log("Largest Palindrome: " + largestPalindrome)
+}
+
+findLargestPalindrome(100, 999)
