d63951d591cdb2c8501658c77d49b0c9a611101d
Bizzle Dapp <36421291+Bizzle-Dapp@users.noreply.github.com>
2020-06-20 15:14:53 +0100

    Wanted to sleep after lunch - instead i wrote this

---
+++ _ProblemCollection/Problem6/p6.js
@@ -0,0 +1,29 @@
+/*
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+
+square = (number) => {
+    return (number * number);
+}
+
+findDifferenceBetweenSumOfSquaresAndSum = (upToNumber) => {
+    let startTime = Date.now();
+    let sumOfSquare = 0;
+    let sum = 0;
+    // Get the sum of the squares and the sum
+    for(let i = 1; i <= upToNumber; i++)
+    {
+        sumOfSquare+=square(i);
+        sum+=i;
+    }
+    // Square the sum
+    sum = square(sum);
+
+    let output = sum > sumOfSquare ? (sum - sumOfSquare) : (sumOfSquare - sum);
+    let endTime = Date.now();
+    console.log("Completed in: " + (endTime - startTime) + " milliseconds.");
+    console.log(output);
+}
+
+findDifferenceBetweenSumOfSquaresAndSum(100);
