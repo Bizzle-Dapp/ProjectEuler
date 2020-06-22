09d3973bcfdbe8a55f3a47a0812b922ac550c04c
bizzle_dapp <lschofield1991@gmail.com>
2020-06-22 13:05:09 +0100

    Was thinking about this problem all morning - solved it, ice cream in hand after lunch. I really liked this one!

---
+++ _ProblemCollection/Problem9/p9.js
@@ -0,0 +1,60 @@
+/*
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a(sq) + b(sq) = c(sq)
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+
+*/
+
+
+let pythagoreanTripletArray = [];
+
+isWholeNumber = (number) => {
+    return ((number - Math.floor(number)) === 0)
+}
+
+// I feel i couldn't have written this without a Magnum Icecream.
+findPythagoreanTripletsUpTo = (number) => {
+    let pythagoreanTriplet = {
+        a : 0,
+        b : 0,
+        c : 0
+    }
+    for(let i = 2; i < (number); i++)
+    {
+        pythagoreanTriplet.a = i;
+        for(let t = (i+1); t < (number); t++)
+        {
+            pythagoreanTriplet.b = t;
+            pythagoreanTriplet.c = (Math.sqrt((i*i) + (t*t)));
+            if(isWholeNumber(pythagoreanTriplet.c))
+            {
+                pythagoreanTripletArray.push({a : pythagoreanTriplet.a, b : pythagoreanTriplet.b, c : pythagoreanTriplet.c});
+            }
+        }
+    }
+}
+
+findProductOfPythagoreanTripletWhereSumOfABCIs = (number) => {
+    let pythagoreanTripletSum = {};
+
+    findPythagoreanTripletsUpTo(number);
+
+    if(pythagoreanTripletArray.length > 0)
+    {
+        pythagoreanTripletArray.forEach(triplet => {
+            if(triplet.a + triplet.b + triplet.c === 1000)
+            {
+                // Bingo
+                pythagoreanTripletSum = triplet;
+            }
+        })
+    }
+
+    console.log(`Product of Triplet that have a sum of ${number} is: ` + (pythagoreanTripletSum.a * pythagoreanTripletSum.b * pythagoreanTripletSum.c));
+}
+
+findProductOfPythagoreanTripletWhereSumOfABCIs(1000);
